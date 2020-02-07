29fcb8cd8fec2dc762b6fb68c42f6cae2647c4cf
przemyslaw-gosciniak <p.gosciniak@o2.pl>
2020-02-07 10:19:14 +0100

    Refactor index.html , new view.

---
+++ src/main/java/org/example/lang/LangService.java
@@ -0,0 +1,25 @@
+package org.example.lang;
+
+import org.example.HibernateUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import static java.util.stream.Collectors.toList;
+
+class LangService {
+    private final Logger logger = LoggerFactory.getLogger(LangService.class);
+    private LangRepository repository;
+
+     LangService(LangRepository repository) {
+        this.repository = repository;
+    }
+    LangService(){
+        this(new LangRepository());
+    }
+
+
+    List<LangDTO> findAll (){
+        return repository.findAll().stream().map(LangDTO::new).collect(toList());
+    }
+}
